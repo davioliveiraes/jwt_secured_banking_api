7120bf9c066132cabb7fa3284b868a220dd47b41
Davi Oliveira <davioliveiraes7@gmail.com>
2026-01-27 11:10:33 -0300

    feat: Adding unit tests in user_repository

--- src/models/repositories/test_user_repository.py
+++ src/models/repositories/test_user_repository.py
@@ -1,15 +1,63 @@
 from src.models.settings.db_connection_handler import db_connection_handler
 from .user_repository import UserRepository
+from unittest.mock import Mock
 
+class MockCursor:
+    def __init__(self) -> None:
+        self.execute = Mock()
+        self.fetchone = Mock()
+    
+class MockConnection:
+    def __init__(self) -> None:
+        self.cursor = Mock(return_value=MockCursor())
+        self.commit = Mock()
+    
 def test_registry_user():
-    db_connection_handler.connect()
-    conn = db_connection_handler.get_connection()
-    repo = UserRepository(conn)
+    username = "Vegetti"
+    password = "omizarieumgenio"
 
-    username = "Patrick"
-    password = "patrickestrela"
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection) # type: ignore
 
-    user = repo.get_user_by_username(username)
-    print()
-    print(user)
+    repo.registry_user(username, password)
+    cursor = mock_connection.cursor.return_value
 
+    assert "INSERT INTO users" in cursor.execute.call_args[0][0]
+    assert "(username, password, balance)" in cursor.execute.call_args[0][0]
+    assert "VALUES" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username, password, 0)
+
+def test_edit_balance():
+    user_id = 3
+    balance = 200.50
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection) # type: ignore
+
+    repo.edit_balance(user_id, balance)
+
+    cursor = mock_connection.cursor.return_value
+
+    assert "UPDATE users" in cursor.execute.call_args[0][0]
+    assert "SET balance = ?" in cursor.execute.call_args[0][0]
+    assert "WHERE id = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (balance, user_id)
+
+    mock_connection.commit.assert_called_once()
+
+def test_get_user_by_username():
+    username = "myUsername"
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection) # type: ignore
+    
+    repo.get_user_by_username(username)
+
+    cursor = mock_connection.cursor.return_value
+
+    assert "SELECT id, username, password" in cursor.execute.call_args[0][0]
+    assert "FROM users" in cursor.execute.call_args[0][0]
+    assert "WHERE username = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username,)
+
+    cursor.fetchone.assert_called_once()
